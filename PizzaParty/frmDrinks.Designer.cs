6b2a1857c7aa3fb0c5680146aa9827a2c991e748
LincT <joseph.dwr.martin@gmail.com>
2017-03-22 13:48:17 -0500

    removed unnecessary event handlers
    removed unnecessary event handlers that were not being used.

--- PizzaParty/frmDrinks.Designer.cs
+++ PizzaParty/frmDrinks.Designer.cs
@@ -39,21 +39,22 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 23);
+            this.label2.Location = new System.Drawing.Point(18, 19);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.Size = new System.Drawing.Size(27, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Size";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(174, 23);
+            this.label3.Location = new System.Drawing.Point(130, 19);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.Size = new System.Drawing.Size(31, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "Type";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // cboSize
             // 
@@ -63,12 +64,11 @@
             "12 oz can - $1",
             "20 oz - $2",
             "2 Liter - $3"});
-            this.cboSize.Location = new System.Drawing.Point(28, 63);
-            this.cboSize.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboSize.Location = new System.Drawing.Point(21, 51);
+            this.cboSize.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cboSize.Name = "cboSize";
-            this.cboSize.Size = new System.Drawing.Size(121, 24);
+            this.cboSize.Size = new System.Drawing.Size(92, 21);
             this.cboSize.TabIndex = 1;
-            this.cboSize.SelectedIndexChanged += new System.EventHandler(this.cboSize_SelectedIndexChanged);
             // 
             // cboType
             // 
@@ -81,18 +81,18 @@
             "Sprite Zero",
             "Orange Fanta",
             "A&W Root Beer"});
-            this.cboType.Location = new System.Drawing.Point(177, 63);
-            this.cboType.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboType.Location = new System.Drawing.Point(133, 51);
+            this.cboType.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cboType.Name = "cboType";
-            this.cboType.Size = new System.Drawing.Size(121, 24);
+            this.cboType.Size = new System.Drawing.Size(92, 21);
             this.cboType.TabIndex = 2;
             // 
             // btnAddDrinks
             // 
-            this.btnAddDrinks.Location = new System.Drawing.Point(55, 131);
-            this.btnAddDrinks.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAddDrinks.Location = new System.Drawing.Point(41, 106);
+            this.btnAddDrinks.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAddDrinks.Name = "btnAddDrinks";
-            this.btnAddDrinks.Size = new System.Drawing.Size(75, 23);
+            this.btnAddDrinks.Size = new System.Drawing.Size(56, 19);
             this.btnAddDrinks.TabIndex = 3;
             this.btnAddDrinks.Text = "Add Drink";
             this.btnAddDrinks.UseVisualStyleBackColor = true;
@@ -100,10 +100,10 @@
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(191, 131);
-            this.btnBack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnBack.Location = new System.Drawing.Point(143, 106);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.Size = new System.Drawing.Size(56, 19);
             this.btnBack.TabIndex = 6;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
@@ -111,19 +111,18 @@
             // 
             // frmDrinks
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(333, 220);
+            this.ClientSize = new System.Drawing.Size(250, 179);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnAddDrinks);
             this.Controls.Add(this.cboType);
             this.Controls.Add(this.cboSize);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmDrinks";
             this.Text = "Drinks";
-            this.Load += new System.EventHandler(this.frmDrinks_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- PizzaParty/frmDrinks.cs
+++ PizzaParty/frmDrinks.cs
@@ -15,33 +15,16 @@ namespace PizzaParty
         public frmDrinks()
         {
             InitializeComponent();
+            cboSize.SelectedIndex = 0;
+            cboType.SelectedIndex = 0;
         }
 
-        private void label3_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void cboSize_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
+       
         private void btnBack_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        private void frmDrinks_Load(object sender, EventArgs e)
-        {
-           
-
-        }
 
         private void btnAddDrinks_Click(object sender, EventArgs e)
         {
@@ -56,11 +39,6 @@ namespace PizzaParty
             //Result to show on txtRunningTotal on Main Form
             this.Tag = drinkSelected;
             this.DialogResult = DialogResult.OK;
-
-            //if and else if statements to determine cost to be added to total
-            if (cboSize.SelectedItem.ToString() == "12 oz can - $1") {/**/ } ;
-
-
         }
     }
 }
